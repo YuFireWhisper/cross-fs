1c7ba149e4215926796f056e596b33bf6865c1e8
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-11-10 08:26:58 +0800

    separate platform-specific sector size implementations

--- src/lib.rs
+++ src/lib.rs
@@ -27,99 +27,99 @@ fn alloc_aligend_buffer(size: usize) -> Vec<u8> {
     }
 }
 
+#[cfg(target_os = "linux")]
 fn get_sector_size() -> usize {
     *SECTOR_SIZE.get_or_init(|| {
-        #[cfg(target_os = "linux")]
-        {
-            use std::os::unix::io::AsRawFd;
+        use std::os::unix::io::AsRawFd;
 
-            let file = std::fs::File::open("/dev/null").expect("Failed to open /dev/null");
-            let fd = file.as_raw_fd();
-            let mut sector_size: u32 = 0;
+        let file = std::fs::File::open("/dev/null").expect("Failed to open /dev/null");
+        let fd = file.as_raw_fd();
+        let mut sector_size: u32 = 0;
 
-            unsafe {
-                assert_ne!(
-                    libc::ioctl(fd, libc::BLKSSZGET, &mut sector_size),
-                    -1,
-                    "Failed to get sector size"
-                );
-            }
-
-            sector_size as usize
-        }
-
-        #[cfg(windows)]
-        {
-            use std::{
-                ffi::{OsStr, c_void},
-                os::windows::ffi::OsStrExt,
-            };
-
-            use windows_sys::Win32::{
-                Foundation::{HANDLE, INVALID_HANDLE_VALUE},
-                Storage::FileSystem::{
-                    CreateFileW, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_SHARE_WRITE,
-                    OPEN_EXISTING,
-                },
-                System::{
-                    IO::DeviceIoControl,
-                    Ioctl::{
-                        IOCTL_STORAGE_QUERY_PROPERTY, PropertyStandardQuery,
-                        STORAGE_ACCESS_ALIGNMENT_DESCRIPTOR, STORAGE_PROPERTY_QUERY,
-                        StorageAccessAlignmentProperty,
-                    },
-                },
-            };
-
-            let path = OsStr::new(DERIVE_PATH)
-                .encode_wide()
-                .chain(std::iter::once(0))
-                .collect::<Vec<u16>>();
-
-            let handle: HANDLE = unsafe {
-                CreateFileW(
-                    path.as_ptr(),
-                    0,
-                    FILE_SHARE_READ | FILE_SHARE_WRITE,
-                    std::ptr::null_mut(),
-                    OPEN_EXISTING,
-                    FILE_ATTRIBUTE_NORMAL,
-                    std::ptr::null_mut(),
-                )
-            };
-
+        unsafe {
             assert_ne!(
-                handle,
-                INVALID_HANDLE_VALUE,
-                "{}",
-                std::io::Error::last_os_error()
+                libc::ioctl(fd, libc::BLKSSZGET, &mut sector_size),
+                -1,
+                "Failed to get sector size"
             );
-
-            let query = STORAGE_PROPERTY_QUERY {
-                PropertyId: StorageAccessAlignmentProperty,
-                QueryType: PropertyStandardQuery,
-                AdditionalParameters: [0],
-            };
-
-            let mut desc: STORAGE_ACCESS_ALIGNMENT_DESCRIPTOR = unsafe { std::mem::zeroed() };
-            let mut bytes_returned: u32 = 0;
-
-            let success = unsafe {
-                DeviceIoControl(
-                    handle,
-                    IOCTL_STORAGE_QUERY_PROPERTY,
-                    &query as *const _ as *mut c_void,
-                    std::mem::size_of::<STORAGE_PROPERTY_QUERY>() as u32,
-                    &mut desc as *mut _ as *mut c_void,
-                    std::mem::size_of::<STORAGE_ACCESS_ALIGNMENT_DESCRIPTOR>() as u32,
-                    &mut bytes_returned,
-                    std::ptr::null_mut(),
-                )
-            };
-
-            assert_ne!(success, 0, "{}", std::io::Error::last_os_error());
-
-            desc.BytesPerLogicalSector as usize
         }
+
+        sector_size as usize
+    })
+}
+
+#[cfg(windows)]
+fn get_sector_size() -> usize {
+    *SECTOR_SIZE.get_or_init(|| {
+        use std::{
+            ffi::{OsStr, c_void},
+            os::windows::ffi::OsStrExt,
+        };
+
+        use windows_sys::Win32::{
+            Foundation::{HANDLE, INVALID_HANDLE_VALUE},
+            Storage::FileSystem::{
+                CreateFileW, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_SHARE_WRITE,
+                OPEN_EXISTING,
+            },
+            System::{
+                IO::DeviceIoControl,
+                Ioctl::{
+                    IOCTL_STORAGE_QUERY_PROPERTY, PropertyStandardQuery,
+                    STORAGE_ACCESS_ALIGNMENT_DESCRIPTOR, STORAGE_PROPERTY_QUERY,
+                    StorageAccessAlignmentProperty,
+                },
+            },
+        };
+
+        let path = OsStr::new(DERIVE_PATH)
+            .encode_wide()
+            .chain(std::iter::once(0))
+            .collect::<Vec<u16>>();
+
+        let handle: HANDLE = unsafe {
+            CreateFileW(
+                path.as_ptr(),
+                0,
+                FILE_SHARE_READ | FILE_SHARE_WRITE,
+                std::ptr::null_mut(),
+                OPEN_EXISTING,
+                FILE_ATTRIBUTE_NORMAL,
+                std::ptr::null_mut(),
+            )
+        };
+
+        assert_ne!(
+            handle,
+            INVALID_HANDLE_VALUE,
+            "{}",
+            std::io::Error::last_os_error()
+        );
+
+        let query = STORAGE_PROPERTY_QUERY {
+            PropertyId: StorageAccessAlignmentProperty,
+            QueryType: PropertyStandardQuery,
+            AdditionalParameters: [0],
+        };
+
+        let mut desc: STORAGE_ACCESS_ALIGNMENT_DESCRIPTOR = unsafe { std::mem::zeroed() };
+        let mut bytes_returned: u32 = 0;
+
+        let success = unsafe {
+            DeviceIoControl(
+                handle,
+                IOCTL_STORAGE_QUERY_PROPERTY,
+                &query as *const _ as *mut c_void,
+                std::mem::size_of::<STORAGE_PROPERTY_QUERY>() as u32,
+                &mut desc as *mut _ as *mut c_void,
+                std::mem::size_of::<STORAGE_ACCESS_ALIGNMENT_DESCRIPTOR>() as u32,
+                &mut bytes_returned,
+                std::ptr::null_mut(),
+            )
+        };
+
+        assert_ne!(success, 0, "{}", std::io::Error::last_os_error());
+
+        desc.BytesPerLogicalSector as usize
     })
 }
